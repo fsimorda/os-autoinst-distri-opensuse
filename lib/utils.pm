8a5db0640ae49e589ee01ed1e4c4f70af1949d85
Frantisek Simorda <frantisek.simorda@suse.com>
2025-11-05 14:52:08 +0100

    S390x schedule yaml for Full flavor

--- lib/utils.pm
+++ lib/utils.pm
@@ -1164,7 +1164,7 @@ without LVM configuration (cr_swap,cr_home etc).
 sub need_unlock_after_bootloader {
     my $is_enc_cc_s390x = check_var('SYSTEM_ROLE', 'Common_Criteria') && check_var('FULL_LVM_ENCRYPT', '1') && is_s390x;
 
-    my $need_unlock_after_bootloader = is_leap('<15.6') || is_sle('<15-sp6') || is_leap_micro || is_sle_micro || (!get_var('LVM', '0') && !get_var('FULL_LVM_ENCRYPT', '0')) || $is_enc_cc_s390x;
+    my $need_unlock_after_bootloader = is_sle('=15-sp7') || is_leap('<15.6') || is_sle('<15-sp6') || is_leap_micro || is_sle_micro || (!get_var('LVM', '0') && !get_var('FULL_LVM_ENCRYPT', '0')) || $is_enc_cc_s390x;
     return 0 if is_boot_encrypted && !$need_unlock_after_bootloader;
     # MicroOS with sdboot supports automatic TPM based unlocking.
     return 0 if is_microos && (is_bootloader_sdboot || is_bootloader_grub2_bls) && get_var('QEMUTPM');
